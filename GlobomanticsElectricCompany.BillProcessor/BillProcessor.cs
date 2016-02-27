2b5c744b90738e5a547f45be7ea30ef891cbf5e2
rlouth-ps-demos <ryan.louth.ps@gmail.com>
2016-02-26 23:42:40 -0600

    commit

---
+++ GlobomanticsElectricCompany.BillProcessor/BillProcessor.cs
@@ -0,0 +1,37 @@
+﻿using GlobomanticsElectricCompany.BillProcessor.Builder;
+
+namespace GlobomanticsElectricCompany.BillProcessor
+{
+    public class BillProcessor
+    {
+        public static void Main()
+        {            
+            //Initialize document object and document builder object
+            
+
+            //Create company contact info
+            CompanyContactInfoBuilder.Build();
+
+            //Create bill details table using Document Builder
+            BillDetailsTableBuilder.Build();
+
+            //Create bill summary table inside a text box
+            BillSummaryTableBuilder.Build();
+
+            //Create historical usage chart
+            HistoricalUsageChartBuilder.Build();
+
+            //Create perforated line for payment stub
+            PaymentStubPerforatedLineBuilder.Build();
+
+            //Create payment stub
+            PaymentStubBuilder.Build();
+
+            //Set global page margins
+            GlobalDocumentSettings.SetPageMargins();
+
+            //Save document
+
+        }
+    }
+}
